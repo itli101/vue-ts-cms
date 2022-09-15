01838aabaddceb54ae2c1c7fc5cde2bb201ecb97
itli101 <wangzhilianluo@163.com>
2022-09-15 09:23:01 +0800

    配置2

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  outputDir: 'build',
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', '@/components')
+  }
+}
